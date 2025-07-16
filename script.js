928a955378b5dbdca31ff4c9516a18ba3f40fe5b
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-07-16 12:45:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ window.addEventListener("load", function () {
+    alert("Welcome to the Netflix Clone. \n\nDisclaimer: This website is for learning purposes only.\n\nMade by Sheikh Hassaan Saeed.");
+  });
+
+ 
